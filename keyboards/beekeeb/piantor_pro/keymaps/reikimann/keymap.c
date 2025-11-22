884afd999b24ddc9493c5dfd4c2d821041d3fa4c
Reikimann <cbirkmand@gmail.com>
2025-11-22 18:34:41 +0100

    Something

--- keyboards/beekeeb/piantor_pro/keymaps/reikimann/keymap.c
+++ keyboards/beekeeb/piantor_pro/keymaps/reikimann/keymap.c
@@ -55,16 +55,95 @@ combo_t key_combos[] = {
     [OE_LATIN] = COMBO(oe_combo, DK_OSTR),
 };
 
+typedef struct {
+    uint16_t tap;
+    uint16_t hold;
+    uint16_t held;
+} tap_dance_tap_hold_t;
+
+void tap_dance_tap_hold_finished(tap_dance_state_t *state, void *user_data) {
+    tap_dance_tap_hold_t *tap_hold = (tap_dance_tap_hold_t *)user_data;
+
+    if (state->pressed) {
+        if (state->count == 1
+#ifndef PERMISSIVE_HOLD
+            && !state->interrupted
+#endif
+        ) {
+            register_code16(tap_hold->hold);
+            tap_hold->held = tap_hold->hold;
+        } else {
+            register_code16(tap_hold->tap);
+            tap_hold->held = tap_hold->tap;
+        }
+    }
+}
+
+void tap_dance_tap_hold_reset(tap_dance_state_t *state, void *user_data) {
+    tap_dance_tap_hold_t *tap_hold = (tap_dance_tap_hold_t *)user_data;
+
+    if (tap_hold->held) {
+        unregister_code16(tap_hold->held);
+        tap_hold->held = 0;
+    }
+}
+
+#define ACTION_TAP_DANCE_TAP_HOLD(tap, hold)                                        \
+    {                                                                               \
+        .fn        = {NULL, tap_dance_tap_hold_finished, tap_dance_tap_hold_reset}, \
+        .user_data = (void *)&((tap_dance_tap_hold_t){tap, hold, 0}),               \
+    }
+
+enum {
+    TD_CP_CTRL,
+    TD_PS_LSFT,
+};
+
+tap_dance_action_t tap_dance_actions[] = {
+    [TD_CP_CTRL] = ACTION_TAP_DANCE_TAP_HOLD(C(KC_C), KC_LCTL),
+    [TD_PS_LSFT] = ACTION_TAP_DANCE_TAP_HOLD(C(KC_V), KC_LSFT),
+};
+
+// --------------------
+// Tapping term per key
+// --------------------
+uint16_t get_tapping_term(uint16_t keycode, keyrecord_t *record) {
+    switch (keycode) {
+        // case <key>:
+        //     return TAPPING_TERM +/- x
+        //     // or
+        //     return x
+        default:
+            return TAPPING_TERM;
+    }
+}
+
 // -------------------
 // Process Record User
 // -------------------
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    tap_dance_action_t *action;
+
     switch (keycode) {
+        case TD(TD_CP_CTRL):
+            action = &tap_dance_actions[QK_TAP_DANCE_GET_INDEX(keycode)];
+            if (!record->event.pressed && action->state.count && !action->state.finished) {
+                tap_dance_tap_hold_t *tap_hold = (tap_dance_tap_hold_t *)action->user_data;
+                tap_code16(tap_hold->tap);
+            }
+            break;
+        case TD(TD_PS_LSFT):
+            action = &tap_dance_actions[QK_TAP_DANCE_GET_INDEX(keycode)];
+            if (!record->event.pressed && action->state.count && !action->state.finished) {
+                tap_dance_tap_hold_t *tap_hold = (tap_dance_tap_hold_t *)action->user_data;
+                tap_code16(tap_hold->tap);
+            }
+            break;
         case SEARCH_IN_NEWTAB:
             if (record->event.pressed) {
                 SEND_STRING(SS_LCTL("ct") SS_DELAY(200) SS_LCTL("lv") "\n");
             };
-        break;
+            break;
     };
     return true;
 };
@@ -81,11 +160,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    *  Pinky:   Alt
    *
    *  ┌───┬───┬───┬───┬───┬───┐       ┌───┬───┬───┬───┬───┬───┐
-   *  │   │ Q │ W │ E │ R │ T │       │ Y │ U │ I │ O │ P │ Å │
+   *  │ ⌀ │ Q │ W │ E │ R │ T │       │ Y │ U │ I │ O │ P │ Å │
    *  ├───┼───┼───┼───┼───┼───┤       ├───┼───┼───┼───┼───┼───┤
-   *  │   │ A │ S │ D │ F │ G │       │ H │ J │ K │ L │ Æ │   │
+   *  │ ⌀ │ A │ S │ D │ F │ G │       │ H │ J │ K │ L │ Æ │ ⌀ │
    *  ├───┼───┼───┼───┼───┼───┤       ├───┼───┼───┼───┼───┼───┤
-   *  │Cap│ Z │ X │ C │ V │ B │       │ N │ M │ , │ . │ - │   │
+   *  │Cap│ Z │ X │ C │ V │ B │       │ N │ M │ , │ . │ - │ ⌀ │
    *  └───┴───┴───┴───┴───┴───┘       └───┴───┴───┴───┴───┴───┘
    *                ┌───┐                   ┌───┐
    *                │Esc├───┐           ┌───┤Del│
@@ -101,11 +180,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
   /*
    *  ┌───┬───┬───┬───┬───┬───┐       ┌───┬───┬───┬───┬───┬───┐
-   *  │   │   │   │   │   │ = │       │ / │ 6 │ 7 │ 9 │ * │   │
+   *  │ 󰇚 │ ⌀ │ ⌀ │ ⌀ │ ⌀ │ = │       │ / │ 6 │ 7 │ 9 │ * │ ⌀ │
    *  ├───┼───┼───┼───┼───┼───┤       ├───┼───┼───┼───┼───┼───┤
-   *  │   │ ▽ │ ▽ │ ▽ │ ▽ │ % │       │ + │ 3 │ 4 │ 5 │ 0 │   │
+   *  │ ⌀ │ ▽ │ ▽ │ ▽ │ ▽ │ % │       │ + │ 3 │ 4 │ 5 │ 0 │ ⌀ │
    *  ├───┼───┼───┼───┼───┼───┤       ├───┼───┼───┼───┼───┼───┤
-   *  │   │   │   │ ( │ ) │ ^ │       │ - │ 1 │ 2 │ 3 │ , │   │
+   *  │ ⌀ │ ⌀ │ ⌀ │ ( │ ) │ ^ │       │ - │ 1 │ 2 │ 3 │ , │ ⌀ │
    *  └───┴───┴───┴───┴───┴───┘       └───┴───┴───┴───┴───┴───┘
    *                ┌───┐                   ┌───┐
    *                │ ▽ ├───┐           ┌───┤ ▽ │
@@ -121,18 +200,18 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
   /*
    *  ┌───┬───┬───┬───┬───┬───┐       ┌───┬───┬───┬───┬───┬───┐
-   *  │   │ ´ │ ' │ # │ % │ - │       │ ` │ { │ @ │ } │ ¨ │   │
+   *  │ ⌀ │ ´ │ ' │ # │ % │ - │       │ ` │ { │ @ │ } │ ¨ │ ⌀ │
    *  ├───┼───┼───┼───┼───┼───┤       ├───┼───┼───┼───┼───┼───┤
-   *  │   │ $ │ \ │ ! │ / │ = │       │ " │ ( │ ? │ ) │ * │   │
+   *  │ ⌀ │ $ │ \ │ ! │ / │ = │       │ " │ ( │ ? │ ) │ * │ ⌀ │
    *  ├───┼───┼───┼───┼───┼───┤       ├───┼───┼───┼───┼───┼───┤
-   *  │   │ § │ < │ & │ > │ + │       │ ~ │ [ │ | │ ] │ ^ │   │
+   *  │ ⌀ │ § │ < │ & │ > │ + │       │ ~ │ [ │ | │ ] │ ^ │ ⌀ │
    *  └───┴───┴───┴───┴───┴───┘       └───┴───┴───┴───┴───┴───┘
    *                ┌───┐                   ┌───┐
    *                │ ▽ ├───┐           ┌───┤ ▽ │
    *                └───┤ ▽ ├───┐   ┌───┤ ▽ ├───┘
    *                    └───┤ ▽ │   │ ▽ ├───┘
    *                        └───┘   └───┘
-   * Missing features:
+   * TODO: Missing features:
    * - Close < on hold
    * - Close ( on hold
    * - Close { on hold
@@ -148,35 +227,35 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
   /*
    *  ┌───┬───┬───┬───┬───┬───┐       ┌───┬───┬───┬───┬───┬───┐
-   *  │   │   │ ★ │ ✖ │ ＋│ ↺ │       │ ↖ │ ⇟ │ ⇞ │ ↘ │   │   │
+   *  │ ⌀ │ ⌀ │ ★ │ ✖ │  │ 󰑓 │       │ ↖ │ ⇟ │ ⇞ │ ↘ │ ⌀ │ ⌀ │
    *  ├───┼───┼───┼───┼───┼───┤       ├───┼───┼───┼───┼───┼───┤
-   *  │   │ ▣ │ ✂ │ ⧉ │ ⎘ │ ↶ │       │ ← │ ↓ │ ↑ │ → │   │   │
+   *  │ ⌀ │ ▣ │ ✂ │ ⧉ │ ⎘ │ ↶ │       │ ← │ ↓ │ ↑ │ → │ ⌀ │ ⌀ │
    *  ├───┼───┼───┼───┼───┼───┤       ├───┼───┼───┼───┼───┼───┤
-   *  │   │   │   │ ⇤ │ ⇥ │ ↷ │       │   │ ⟸ │ ⟹ │   │   │   │
+   *  │ ⌀ │ ⌀ │ ⌀ │ ⇤ │ ⇥ │ ↷ │       │ ⌀ │ ⟸ │ ⟹ │ ⌀ │ ⌀ │ ⌀ │
    *  └───┴───┴───┴───┴───┴───┘       └───┴───┴───┴───┴───┴───┘
    *                ┌───┐                   ┌───┐
    *                │   ├───┐           ┌───┤   │
    *                └───┤   ├───┐   ┌───┤   ├───┘
    *                    └───┤   │   │   ├───┘
    *                        └───┘   └───┘
-   * Missing features:
+   * TODO: Missing features:
    * - Shift when holding paste button
    * - Ctrl when holding copy button
    *
    */
   [_NAVIGTION] = LAYOUT_split_3x6_3(
-    KC_NO, KC_NO, SEARCH_IN_NEWTAB, C(KC_W),     C(KC_T),   LCS(KC_T),                       KC_HOME, KC_PGDN, KC_PGUP, KC_END,  KC_NO, KC_NO,
-    KC_NO, C(KC_A), C(KC_X),        C(KC_C),     C(KC_V),   C(KC_Z),                         KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_NO, KC_NO,
-    KC_NO, KC_NO, KC_NO,            LCS(KC_TAB), C(KC_TAB), C(KC_Y),                         KC_NO, KC_WBAK, KC_WFWD, KC_NO, KC_NO, KC_NO,
-                                                    _______, _______, _______,      _______, _______, _______
+    KC_NO, KC_NO, SEARCH_IN_NEWTAB, C(KC_W),        C(KC_T),        LCS(KC_T),                       KC_HOME, KC_PGDN, KC_PGUP, KC_END,  KC_NO, KC_NO,
+    KC_NO, C(KC_A), C(KC_X),        TD(TD_CP_CTRL), TD(TD_PS_LSFT), C(KC_Z),                         KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_NO, KC_NO,
+    KC_NO, KC_NO, KC_NO,            LCS(KC_TAB),    C(KC_TAB),      C(KC_Y),                         KC_NO, KC_WBAK, KC_WFWD, KC_NO, KC_NO, KC_NO,
+                                                        _______, _______, _______,      _______, _______, _______
   ),
   /*
    *  ┌───┬───┬───┬───┬───┬───┐       ┌───┬───┬───┬───┬───┬───┐
-   *  │   │   │   │   │   │   │       │   │   │   │   │   │   │
+   *  │ ⌀ │ ⌀ │  │ 󰒮 │ 󰒭 │ 󰏤 │       │ ⌀ │ F7│ F8│ F9│F12│ ⌀ │
    *  ├───┼───┼───┼───┼───┼───┤       ├───┼───┼───┼───┼───┼───┤
-   *  │   │   │   │   │   │   │       │   │   │   │   │   │   │
+   *  │ ⌀ │SFT│  │ 󰝞 │ 󰝝 │ 󰝟 │       │ ⌀ │ F4│ F5│ F6│F11│ ⌀ │
    *  ├───┼───┼───┼───┼───┼───┤       ├───┼───┼───┼───┼───┼───┤
-   *  │   │   │   │   │   │   │       │   │   │   │   │   │   │
+   *  │ ⌀ │ ⌀ │  │ 󰃞 │ 󰃠 │ ⌀ │       │ ⌀ │ F1│ F2│ F3│F10│ ⌀ │
    *  └───┴───┴───┴───┴───┴───┘       └───┴───┴───┴───┴───┴───┘
    *                ┌───┐                   ┌───┐
    *                │   ├───┐           ┌───┤   │
@@ -197,11 +276,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
   /*
    *  ┌───┬───┬───┬───┬───┬───┐       ┌───┬───┬───┬───┬───┬───┐
-   *  │   │   │   │   │   │   │       │   │   │   │   │   │   │
+   *  │ ⌀ │ ⌀ │ ⌀ │ ⌀ │ ⌀ │ ⌀ │       │   │   │   │   │   │   │
    *  ├───┼───┼───┼───┼───┼───┤       ├───┼───┼───┼───┼───┼───┤
-   *  │   │   │   │   │   │   │       │   │   │   │   │   │   │
+   *  │ ⌀ │ ⌀ │ ⌀ │ ⌀ │ ⌀ │ ⌀ │       │   │   │   │   │   │   │
    *  ├───┼───┼───┼───┼───┼───┤       ├───┼───┼───┼───┼───┼───┤
-   *  │   │   │   │   │   │   │       │   │   │   │   │   │   │
+   *  │ ⌀ │ ⌀ │ ⌀ │ ⌀ │ ⌀ │ ⌀ │       │   │   │   │   │   │   │
    *  └───┴───┴───┴───┴───┴───┘       └───┴───┴───┴───┴───┴───┘
    *                ┌───┐                   ┌───┐
    *                │   ├───┐           ┌───┤   │
